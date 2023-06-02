5efccfe128df7240ae1488520c82edf99a9138e3
Tosif <md.tosif@zeeve.io>
2023-06-02 14:36:10 +0530

    null checks

--- src/utils/Operator.mjs
+++ src/utils/Operator.mjs
@@ -2,8 +2,8 @@ import _ from 'lodash'
 
 const Operator = (network, data) => {
   const { address } = data
-  const botAddress = data.restake.address
-  const minimumReward = data.restake.minimum_reward
+  const botAddress = data.restake?.address
+  const minimumReward = data.restake?.minimum_reward
 
   return {
     address,
